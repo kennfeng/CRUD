8fe02b089b2011fbbbcf4bd5235b0747e9819a1f
kenn <kenn-feng@outlook.com>
2024-10-28 03:27:08 -0400

    Search functionality

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "P Search Engine!"
 
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    return jsonify({"message": "Search functionality to be implemented!"})
+
 if __name__ == "__main__":
     app.run(debug=True)
