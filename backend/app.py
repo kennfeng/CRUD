151b6645d531bbd2974e1d98f56011a7b95440b3
kenn <kenn-feng@outlook.com>
2024-10-30 12:45:18 -0400

    docs

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,8 @@ from CRUD import crud_bp
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # SQLite database file
+    # database file
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
